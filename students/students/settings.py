a8384e29c235b0f15c6084645a1d893a24982854
s41118143 <148747612+s41118143@users.noreply.github.com>
2025-04-15 14:41:23 +0800

    Add files via upload

--- students/students/settings.py
+++ students/students/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-18=w9az+vrgj*ssr*mm5w(p#zh5)5%(%y)65_xmwelba1u33(^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
